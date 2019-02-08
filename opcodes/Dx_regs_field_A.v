ccd373243f3bc706b27c44224e74e2c08689f49f
Raphael Jacquot <raphael.jacquot@univ-grenoble-alpes.fr>
2019-02-08 23:59:56 +0100

    implement a couple mode opcodes

---
+++ opcodes/Dx_regs_field_A.v
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * Dx
+ * register manipulation field A
+ *
+ */ 
+
+
+`include "decstates.v"
+
+`DEC_DX: begin
+    case (nibble)
+    4'hA: begin
+        A[19:0] <= C[19:0];
+        $display("%5h A=C\tA", inst_start_PC);
+    end
+    4'hE: begin
+        A[19:0] <= C[19:0];
+        C[19:0] <= A[19:0];
+        $display("%5h ACEX\tA", inst_start_PC);
+    end
+    default: begin 
+        $display("ERROR : DEC_DX");
+        decode_error <= 1;    
+    end
+    endcase
+    decstate <= `DEC_START;
+end
