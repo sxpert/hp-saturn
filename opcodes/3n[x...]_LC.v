aa95324ea99771ed4fd60b4d8b0a761f8d7d3134
Raphael Jacquot <raphael.jacquot@univ-grenoble-alpes.fr>
2019-02-11 07:03:20 +0100

    implement LC with ALU operations (need to find a way to output the instruction representation)

--- opcodes/3n[x...]_LC.v
+++ opcodes/3n[x...]_LC.v
@@ -5,23 +5,22 @@
  */ 
 
 `include "decstates.v"
+`include "fields.v"
 
-`DEC_LC_LEN: begin
-    t_cnt <= nb_in;
-    t_ctr <= 0;
-    decstate <= `DEC_LC;
-end
 `DEC_LC: begin
-    C[((t_ctr+P)%16)*4+:4] <= nb_in;
-    if (t_ctr == t_cnt) begin
-        decstate <= `DEC_START;
-`ifdef SIM
-        $write("%5h LC (%h)\t%1h", inst_start_PC, t_cnt, nb_in);
-        for(t_ctr = 0; t_ctr != t_cnt; t_ctr ++)
-            $write("%1h", C[(((t_cnt - t_ctr - 4'h1)+P)%16)*4+:4]);
-        $write("\n");
-`endif
-    end else begin 
-        t_ctr <= (t_ctr + 1)&4'hf;
-    end							
+    alu_first <= P;
+    alu_last <= (P + nb_in) & 4'hF;
+    alu_reg_src1 <= `ALU_REG_MEM;
+    alu_reg_dest <= `ALU_REG_C;
+    alu_op <= `ALU_OP_COPY;
+
+    // alu_debug <= 1;
+    decstate <= `DEC_ALU_INIT;
+    alu_return <= `DEC_START;
+// `ifdef SIM
+//         $write("%5h LC (%h)\t%1h", inst_start_PC, t_cnt, nb_in);
+//         for(t_ctr = 0; t_ctr != t_cnt; t_ctr ++)
+//             $write("%1h", C[(((t_cnt - t_ctr - 4'h1)+P)%16)*4+:4]);
+//         $write("\n");
+// `endif
 end
