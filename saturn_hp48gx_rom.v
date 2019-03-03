2cab45a6ffd1dc4a4f44af43d0af2236076264fb
Raphaël Jacquot <sxpert@mac2.sxpert.org>
2019-03-03 10:08:26 +0100

    remove the reset on the rom access, doesn't work

--- saturn_hp48gx_rom.v
+++ saturn_hp48gx_rom.v
@@ -92,9 +92,6 @@ assign access_pointer = do_pc_read?local_pc[`ROMBITS-1:0]:local_dp[`ROMBITS-1:0]
 always @(posedge i_clk) begin
     if (i_bus_clk_en && i_bus_is_data && do_read)
         o_bus_nibble_out <= rom_data[access_pointer];
-
-    if (i_reset)
-        o_bus_nibble_out <= 4'b0;
 end
 
 /*
